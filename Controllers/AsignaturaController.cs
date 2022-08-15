4d0ca4ed3ea00e23aa13e403cae05591fb2def5b
Cesar Choquehuanca Delgado <cesarchoquehuancadelgado@MBP-de-Cesar.home>
2022-08-15 18:23:18 -0500

    Vistas Parciales

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -6,18 +6,48 @@ namespace escuela_aspnet_core_app.Controllers
 {
     public class AsignaturaController : Controller
     {
+
         public IActionResult Index()
         {
-            var asignatura = new Asignatura()
+            ViewBag.Fecha = DateTime.Now;
+
+            return View(new Asignatura
             {
-                UniqueId = Guid.NewGuid().ToString(),
-                Nombre = "Programación"
+                Nombre = "Matemáticas",
+                UniqueId = Guid.NewGuid().ToString()
+            });
+        }
+
+
+            public IActionResult MultiAsignatura()
+        {
+            var listaAsignaturas = new List<Asignatura>() {
+                new Asignatura {
+                Nombre = "Matemáticas",
+                UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                Nombre = "Educación Física",
+                UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                Nombre = "Castellano",
+                UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                Nombre = "Ciencias Naturales",
+                UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                Nombre = "Programacion",
+                UniqueId = Guid.NewGuid ().ToString ()
+                }
             };
 
             ViewBag.CosaDinamica = "La Monja Asignatura";
             ViewBag.Fecha = DateTime.Now;
 
-            return View(asignatura);
+            return View("MultiAsignatura", listaAsignaturas);
         }
     }
 }
