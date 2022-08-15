34446b4b50a58500d8887f8002c9eae5a942daa0
Cesar Choquehuanca Delgado <cesarchoquehuancadelgado@MBP-de-Cesar.home>
2022-08-15 00:34:17 -0500

    Modelo a nivel basico, Vista y controlador

---
+++ Controllers/EscuelaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using escuela_aspnet_core_app.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace escuela_aspnet_core_app.Controllers
+{
+    public class EscuelaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var escuela = new Escuela();
+            escuela.AñoFundacion = 2005;
+            escuela.EscuelaID = Guid.NewGuid().ToString();
+            escuela.Nombre = "Platzi School";
+            return View(escuela);
+        }
+    }
+}
+
---
+++ Models/Escuela.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace escuela_aspnet_core_app.Models
+{
+    public class Escuela
+    {
+        public string EscuelaID { get; set; }
+        public string Nombre { get; set; }
+        public int AñoFundacion { get; set; }
+    }
+}
+
