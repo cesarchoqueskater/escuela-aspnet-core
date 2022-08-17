afec88d82c5252f95b4b6877e904bbf8f2523e75
Cesar Choquehuanca Delgado <cesarchoquehuancadelgado@MBP-de-Cesar.home>
2022-08-16 19:22:20 -0500

    Actualizacion version de Boostrap, invocacion de lista controladores

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -1,53 +1,46 @@
 ﻿using System;
+using System.Linq;
 using escuela_aspnet_core_app.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace escuela_aspnet_core_app.Controllers
 {
-    public class AsignaturaController : Controller
+    public class AlumnoController : Controller
     {
 
         public IActionResult Index()
         {
             ViewBag.Fecha = DateTime.Now;
 
-            return View(new Asignatura
+            return View(new Alumno
             {
-                Nombre = "Matemáticas",
+                Nombre = "Cesar",
                 UniqueId = Guid.NewGuid().ToString()
             });
         }
 
 
-            public IActionResult MultiAsignatura()
+        public IActionResult MultiAlumno()
         {
-            var listaAsignaturas = new List<Asignatura>() {
-                new Asignatura {
-                Nombre = "Matemáticas",
-                UniqueId = Guid.NewGuid ().ToString ()
-                },
-                new Asignatura {
-                Nombre = "Educación Física",
-                UniqueId = Guid.NewGuid ().ToString ()
-                },
-                new Asignatura {
-                Nombre = "Castellano",
-                UniqueId = Guid.NewGuid ().ToString ()
-                },
-                new Asignatura {
-                Nombre = "Ciencias Naturales",
-                UniqueId = Guid.NewGuid ().ToString ()
-                },
-                new Asignatura {
-                Nombre = "Programacion",
-                UniqueId = Guid.NewGuid ().ToString ()
-                }
-            };
+            var listaAlumno = GenerarAlumnosAlAzar();
 
-            ViewBag.CosaDinamica = "La Monja Asignatura";
-            ViewBag.Fecha = DateTime.Now;
 
-            return View("MultiAsignatura", listaAsignaturas);
+            return View("MultiAlumno", listaAlumno);
+        }
+
+        private List<Alumno> GenerarAlumnosAlAzar()
+        {
+            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+
+            var listaAlumnos = from n1 in nombre1
+                               from n2 in nombre2
+                               from a1 in apellido1
+                               select new Alumno { Nombre = $"{n1} {n2} {a1}" ,
+                                                  UniqueId = Guid.NewGuid().ToString()};
+
+            return listaAlumnos.OrderBy((al) => al.UniqueId).ToList();
         }
     }
 }
